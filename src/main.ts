835a613af8a8d56799e236a5635f1e5e0ef6430a
daniildzk <freekick2017uk@gmail.com>
2024-02-03 18:21:12 +0200

    changes

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.use(
     cors({
       credentials: true,
-      origin: true,
+      origin: ["http://localhost:3000"],
     }),
   );
   app.use(cookieParser());
